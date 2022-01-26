e4dd60b9fb321f66e578e5af6bbaad37b282422a
William Spacefire <william@meycup.com>
2022-01-25 21:47:14 -0300

    Fixed the center problem

--- src/views/post/PostTitle.tsx
+++ src/views/post/PostTitle.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { Heading } from '@chakra-ui/react'
+import * as React from 'react'
+import { Center, Heading } from '@chakra-ui/react'
 
 export default class PostTitle extends React.Component<{ title: string }> {
     render() {
         return (
-            <center>
-                <Heading
-                    as='h1'
-                    margin='3'
-                    fontSize='2.4rem'
-                    fontWeight='extrabold'>
-                    {this.props.title}
-                </Heading>
-            </center>
+            <>
+                <Center>
+                    <Heading
+                        as='h1'
+                        margin='3'
+                        fontSize='2.4rem'
+                        fontWeight='extrabold'>
+                        {this.props.title}
+                    </Heading>
+                </Center>
+            </>
         )
     }
 }
